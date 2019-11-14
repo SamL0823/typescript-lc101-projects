3959c7a20c99cb48f287f10dfc0bb5b2332dc784
Sam <samleo0823@gmail.com>
2019-11-13 19:40:54 -0600

    first commit

--- exercises/SpaceLocation.ts
+++ exercises/SpaceLocation.ts
@@ -1,1 +1,11 @@
 // Paste in the provided code here:
+export class SpaceLocation {
+    name: string;
+    kilometersAway: number;
+    
+
+    constructor(name: string, kilometersAway: number) {
+        this.name = name;
+        this.kilometersAway = kilometersAway;
+    }
+}
